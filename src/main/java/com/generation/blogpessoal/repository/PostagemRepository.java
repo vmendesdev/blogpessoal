94eae3c2d2e97cfc23144662019ab6a03b0ebb32
Vanessa Mendes <vmendesjob@gmail.com>
2024-08-20 11:20:40 -0300

    exclusão de comentários

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -6,11 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.blogpessoal.model.Postagem;
 
-//public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-
-	//public List <Postagem> findAllByTituloContainingTgnoreCase(@Param("titulo") String titulo);
-//}
-
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
     List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 }
